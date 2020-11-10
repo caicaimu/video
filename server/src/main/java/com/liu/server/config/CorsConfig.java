4870020cda177fcee201a57cdecea59b06c96ad1
zzz <2606173329@qq.com>
2020-11-10 21:13:37 +0800

    1:集成axios完成前后端交互 2: 增加CorsConfig 解决前后端跨域问题

--- server/src/main/java/com/liu/server/config/CorsConfig.java
+++ server/src/main/java/com/liu/server/config/CorsConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders(CorsConfiguration.ALL)
