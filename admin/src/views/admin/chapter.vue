661a84a7d61fe96dc4ab7a14e937f63cd6b3b97e
zzz <2606173329@qq.com>
2020-11-13 10:50:16 +0800

    1:集成sweetalert2,删除弹出确认框 2:制作toast组件 使用sweetalert2实现 执行方法成功之后 弹出一个提示框 3:制作confirm组件 简化弹出框

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -106,6 +106,7 @@
                 _this.chapter={};
                 $("#form-modal").modal("show");
             },
+
             /**
              * 修改
              * */
@@ -122,12 +123,14 @@
              */
             list(page){
                 let _this =this;
+                Loading.show();
                 _this.$ajax.post(
                     "http://127.0.0.1:9000/business/admin/chapter/list",
                     {
                         page:page,
                         size:_this.$refs.pagination.size,
                     }).then((response)=>{
+                    Loading.hide();
                     console.log("查询大章的结果",response)
                     let resp=response.data;
                     _this.chapters = resp.content.list;
@@ -140,13 +143,16 @@
              */
             save(page){
                 let _this =this;
+                Loading.show();
                 _this.$ajax.post(
                     "http://127.0.0.1:9000/business/admin/chapter/save", _this.chapter).then((response)=>{
                     console.log("查询大章的结果",response);
-                    let resp=response.data;
+                    let resp=response.data
+                    Loading.hide();
                     if (resp.success){
                         $("#form-modal").modal("hide");
                         _this.list(1);
+                        Toast.success("保存成功");
                     }
                 })
             },
@@ -156,12 +162,17 @@
              */
             del(id){
                 let _this =this;
-                _this.$ajax.delete(
-                    "http://127.0.0.1:9000/business/admin/chapter/delete/"+ id).then((response)=>{
-                    let resp=response.data;
-                    if (resp.success){
-                        _this.list(1);
-                    }
+                Confirm.show("删除后不可以恢复,确认删除？" ,function () {
+                    Loading.show();
+                    _this.$ajax.delete(
+                        "http://127.0.0.1:9000/business/admin/chapter/delete/"+ id).then((response)=>{
+                        let resp=response.data;
+                        Loading.hide();
+                        if (resp.success){
+                            _this.list(1);
+                            Toast.success("删除成功");
+                        }
+                    })
                 })
             },
         }
